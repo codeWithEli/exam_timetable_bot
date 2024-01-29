0c172e75c9f372802e434952218ffe4e2f5aac78
eli-bigman <nutsugah81@gmail.com>
2024-01-29 21:50:29 +0000

    initial commit

---
+++ src/scraper.py
@@ -0,0 +1,30 @@
+
+import os
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+
+
+
+
+
+class Scraper:
+    """Scrapper class"""
+    def __init__(self):
+        """Initialize chrome browser and scrapes the website"""
+
+    def get_page():
+        pass
+
+    def parse_html():
+        pass
+
+    def extract_timetable():
+        pass
+
+    def run_scraper():
+        pass
+
+    def get_exams_timetable():
+        pass
+    
