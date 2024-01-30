13fd6f4dcfb335ae25b280cbc0291290252937f2
eli-bigman <nutsugah81@gmail.com>
2024-01-30 00:11:25 +0000

    error extracting schedule link

--- src/scraper.py
+++ src/scraper.py
@@ -1,30 +1,70 @@
 
 import os
+import time
+
+import dotenv 
+import requests
+
 from bs4 import BeautifulSoup
-from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome  import ChromeDriverManager
 
 
 
-
-
+dotenv.load_dotenv()
+URL = os.getenv("URL")
 
 class Scraper:
     """Scrapper class"""
     def __init__(self):
-        """Initialize chrome browser and scrapes the website"""
+        """Initialize chrome browser"""
 
-    def get_page():
+        service = Service(ChromeDriverManager().install())
+        options = webdriver.ChromeOptions() 
+
+        #browser options
+        options.add_experimental_option("detach", True)
+        
+
+        self.driver = webdriver.Chrome(service=service, options=options)
+        self.driver.set_window_size(2024,1020)
+        self.driver.implicitly_wait(7)
+
+       
+    
+
+
+    def get_page(self):
+        try:
+            self.driver.get(URL)    
+
+            #click on schedule generator
+            element =  self.driver.find_element(By.XPATH, "//li//a[contains(text(),'Schedule Generator')]")
+
+            print(element.text)
+            
+            
+            
+            #search for the course
+
+
+
+        except Exception as e:
+            print(str(e))
+
+    def parse_html(self):
         pass
 
-    def parse_html():
+    def extract_timetable(self):
         pass
 
-    def extract_timetable():
-        pass
-
-    def run_scraper():
-        pass
-
-    def get_exams_timetable():
+    def get_requested_timetable(self):
         pass
     
+
+if __name__ == '__main__':
+    scraper = Scraper()
+    scraper.get_page()
