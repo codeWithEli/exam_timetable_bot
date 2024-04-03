0ad323b63a0d049994bb4d5041e67d61be73698b
eli-bigman <nutsugah81@gmail.com>
2024-04-03 01:49:28 +0000

    ignored future warnings

--- src/alarm.py
+++ src/alarm.py
@@ -1,9 +1,11 @@
-import random
 import logging
 import firebase_functions as FB
 from ics import Calendar, Event, DisplayAlarm
 from datetime import datetime, timedelta
+import warnings
 
+
+warnings.simplefilter("ignore", FutureWarning)
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -16,28 +18,25 @@ def create_alarm_file(user_id: str, alarm_offset_minutes: int) -> str:
 
     try:
         all_exams_details = FB.get_saved_exams_details(user_id)
-        
-        cal = Calendar()
 
-        for course_name, course_info in all_exams_details.items():all_exams_details = FB.get_saved_exams_details(user_id)
-        
         cal = Calendar()
 
         for course_name, course_info in all_exams_details.items():
             # Create event
             event = Event()
-            event.name = course_info["Full_Course_Name"]
-            event.description = f"ALL EXAMS VENUES: {course_info['All_Exams_Venue']}"
+            event.name = course_info.get("Full_Course_Name")
+            event.description = f"ALL EXAMS VENUES: {course_info.get('All_Exams_Venue')}"
             event.begin = datetime.strptime(
-                f"{course_info['Exams_Date']} {course_info['Exams_Time']}", "%B %d, %Y %I:%M %p")
+                f"{course_info.get('Exams_Date')} {course_info.get('Exams_Time')}", "%B %d, %Y %I:%M %p")
 
             # Construct display text and venue
+
             display_text = f"{course_info['Full_Course_Name']}"
             Venue = ""
-            if course_info["Exact_Exams_Venue"] is not None:
-                Venue = f"{course_info['Exact_Exams_Venue']}"
+            if course_info.get("Exact_Exams_Venue"):
+                Venue = f"{course_info.get('Exact_Exams_Venue')}"
             else:
-                Venue = f"{', '.join(course_info['All_Exams_Venue'])}"
+                Venue = f"{', '.join(course_info.get('All_Exams_Venue'))}"
 
             event.location = Venue
 
@@ -51,18 +50,16 @@ def create_alarm_file(user_id: str, alarm_offset_minutes: int) -> str:
             cal.events.add(event)
 
         # Create a single ICS file with all events
-        ran_num = random.randint(0, 1000)
         now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        filename = f"{ran_num}-Exam_Reminder-{user_id}-{now}.ics"
+        filename = f"Exam_Reminder-{user_id}-{now}.ics"
         filename_path = f"./{filename}"
 
         with open(filename_path, "w") as f:
             f.writelines(cal.serialize())
-            # f.writelines(alarm.serialize())
 
         calendar_url = FB.upload_calendar_to_firebase(filename_path, filename)
 
-        logger.info(f"All exam alarm information saved to: {filename}")
+        logger.info(f"Exam alarm information saved to: {filename}")
 
         return calendar_url
 
