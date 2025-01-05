b2078e8259828cdf11f776faa8e9e1ec4d86842b
elibigman <nutsugah81@gmail.com>
2025-01-05 02:02:11 +0000

    chore: update dependencies and clean up requirements.txt
    - Upgraded Selenium to 4.27.1
    - Updated webdriver-manager to 4.0.2
    - Removed prettyTables dependency
    - Added venv to gitignore
    - Fixed webhook environment variable typo

---
+++ space.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+# Explicitly specify the path to the .env file
+dotenv_path = find_dotenv('.env')
+load_dotenv(dotenv_path)
+print(dotenv_path)
+
+# Get the bot token from environment variables
+TOKEN = os.getenv("BOT_TOKEN")
+
+if TOKEN:
+    TOKEN = TOKEN.strip()
+    space_dict = {index: char.isspace() for index, char in enumerate(TOKEN)}
+    print(space_dict)
+else:
+    print("BOT_TOKEN environment variable is not set.")
